4f3cacadc30a9016c41e093aa9e9755569e125c4
eatradish <sakiiily@aosc.io>
2019-09-10 23:44:51 +0800

    GIthubInterface: add Issue

--- github/GithubInterface.ts
+++ github/GithubInterface.ts
@@ -1,4 +1,59 @@
 export interface LoginRequest {
-    username: string,
-    password: string,
+    username: string;
+    password: string;
+}
+
+export interface Issue {
+    url: string;
+    repository_url: string;
+    comments_url: string;
+    events_url: string;
+    html_url: string;
+    id: number;
+    node_id: string;
+    number: number;
+    title: string;
+    user: User;
+    label: IssueLabel[];
+    state: string;
+    locked: boolean;
+    assignee: User;
+    assignees: User[];
+    milestone: {};
+    comments: number;
+    created_at: string;
+    updated_at?: string;
+    closed_at?: string;
+    author_association: string;
+    body: string;
+}
+
+export interface User {
+    login: string;
+    id: number;
+    node_id: string;
+    avatar_url: string;
+    gravatar_id: string;
+    url: string;
+    html_url: string;
+    followers_url: string;
+    following_url: string;
+    gists_url: string;
+    starred_url: string;
+    subscriptions_url: string;
+    organizations_url: string;
+    repos_url: string;
+    events_url: string;
+    received_events_url: string;
+    type: string;
+    site_admin: boolean;
+}
+
+export interface IssueLabel {
+    id: number;
+    node_id: string;
+    url: string;
+    name: string;
+    color: string;
+    default: boolean;
 }
--- github/bot.ts
+++ github/bot.ts
@@ -3,7 +3,7 @@ import Axios, { AxiosInstance } from 'axios';
 import * as GithubInterface from './GithubInterface';
 import axiosRetry from 'axios-retry';
 
-function readLoginInfo(): GithubInterface.LoginRequest | undefined {
+function readLoginInfo(): GithubInterface.LoginRequest {
     const file = Fs.readFileSync('./login.json', 'utf8');
     if (file) return JSON.parse(file) as GithubInterface.LoginRequest;
     else throw new Error('File not exist');
@@ -21,25 +21,30 @@ class GithubBot {
     }
     public async login() {
         const res = await this.requester.get('/', { auth: this.auth });
-        if (res.status === 200) console.log(res.data);
+        if (res.status === 200 && res.data) return res.data;
         else throw new Error('login failed');
     }
     public async getInfo() {
         const res = await this.requester.get('');
-        if (res.status === 200) console.log(res.data);
+        if (res.status === 200 && res.data) return res.data;
+        else throw new Error('GET info failed');
     }
     public async getCloseIssue() {
         const res = await this.requester.get('/repos/aosc-dev/aosc-os-abbs/issues', { 
             params: { state: 'closed' },
         });
-        if (res.status === 200) console.log(res.data);
+        if (res.status === 200 && res.data) return res.data;
+        else throw new Error('GET close issue failed');
     }
 }
-const login = readLoginInfo();
-if (login) {
-    const bot = new GithubBot(login);
+
+const main = async () => {
+    const bot = new GithubBot(readLoginInfo());
     bot.login();
-    bot.getCloseIssue();
+    console.log((await bot.getCloseIssue())[0]);
 }
+
+main();
+
 
 
