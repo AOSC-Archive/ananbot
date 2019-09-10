20382d8091ff6bc7686ecc15d921e55feb0ec205
eatradish <sakiiily@aosc.io>
2019-09-11 00:57:47 +0800

    github_bot: add openNewIssue()

--- github/bot.ts
+++ github/bot.ts
@@ -11,37 +11,41 @@ function readLoginInfo(): GithubInterface.LoginRequest {
 
 class GithubBot {
     private requester: AxiosInstance;
-    private auth: GithubInterface.LoginRequest;
     public constructor(login: GithubInterface.LoginRequest) {
         this.requester = Axios.create({
             baseURL: "https://api.github.com",
+            auth: login,
         });
-        this.auth = login;
         axiosRetry(this.requester, { retries: 3 });
     }
-    public async login() {
-        const res = await this.requester.get('/', { auth: this.auth });
-        if (res.status === 200 && res.data) return res.data;
-        else throw new Error('login failed');
-    }
-    public async getInfo() {
+    public async getInfo(): Promise<GithubInterface.LoginResponse> {
         const res = await this.requester.get('');
         if (res.status === 200 && res.data) return res.data;
         else throw new Error('GET info failed');
     }
-    public async getCloseIssue() {
-        const res = await this.requester.get('/repos/aosc-dev/aosc-os-abbs/issues', { 
+    public async getCloseIssue(): Promise<GithubInterface.IssueResponse> {
+        const res = await this.requester.get('/repos/eatradish/saki-telebot-api/issues', { 
             params: { state: 'closed' },
         });
         if (res.status === 200 && res.data) return res.data;
         else throw new Error('GET close issue failed');
     }
+    public async openNewIssue(issue: GithubInterface.OpenNewIssueRequest) {
+        const json = JSON.stringify(issue);
+        const res = await this.requester.post('/repos/eatradish/saki-telebot-api/issues', json);
+        if (res.status === 201 && res.data) return res.data;
+        else throw new Error('POST open new issue failed');
+    }
 }
 
 const main = async () => {
     const bot = new GithubBot(readLoginInfo());
-    bot.login();
-    console.log((await bot.getCloseIssue())[0]);
+    //console.log(await bot.getInfo());
+    //onsole.log((await bot.getCloseIssue()));
+    console.log(await bot.openNewIssue({
+        title: "qaq",
+        body: "I'm having a problem with this.",
+    }));
 }
 
 main();
