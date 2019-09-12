8d2efc79439e3a276f2bb9759656006c8d283ab6
eatradish <sakiiily@aosc.io>
2019-09-12 21:22:51 +0800

    index: add bot func

--- index.ts
+++ index.ts
@@ -17,13 +17,33 @@ const main = async () => {
     const teleramBot = createTelegramBot();
 
     teleramBot.on('/hello', (msg: Messages.default) => msg.replyText('Hello'));
-    teleramBot.on(/^\/openissue (.+)$/, (msg: Messages.default, props: string[]) => {
+    teleramBot.on(/^\/openissue (.+)$/, async (msg: Messages.default, props: string[]) => {
         if (!msg.obj.reply_to_message || !msg.obj.message || !msg.obj.message.text) return;
         const title = props[1];
         const body = msg.obj.message.text;
         const issue = { title, body };
-        githubBot.openNewIssue(issue);
+        const res = await githubBot.openNewIssue(issue);
+        if (res) msg.replyText(`Open issue success, issue number: ${res.number}`);
     });
+
+    teleramBot.on(/^\/closeissue (.+)$/, async (msg: Messages.default, props: string[]) => {
+        let issue: number = -1;
+        if (Number(props[1]) !== NaN) {
+            issue = Number(props[1])
+        }
+        else if (props[1].indexOf('github.com') !== -1) {
+            const parseList = props[1].split('/');
+            if (Number(parseList[parseList.length - 1]) !== NaN) issue = Number(parseList[parseList.length - 1]);
+        }
+        else if (props[1][0] === '#') {
+            const toNumber = props[1].replace('#', '');
+            if (Number(toNumber) !== NaN) issue = Number(toNumber);
+        }
+        if (issue === -1) throw new Error('arg Error');
+        const res = await githubBot.closeIssue(issue);
+        if (res) msg.replyText(`Close issue success, issue number: ${res.number}`);
+    });
+    
     teleramBot.listen();
 }
 
