fcd370fffaff2e53409f0ad93a7a4f680c9a06dd
eatradish <sakiiily@aosc.io>
2019-09-17 12:52:45 +0800

    telegram: fix title slice problem

--- index.ts
+++ index.ts
@@ -13,18 +13,24 @@ const main = async (): Promise<void> => {
     const telegramBot = await createTelegramBot();
     telegramBot.on('/hello', (message: TDLTypes.message) => telegramBot.sendMessage(message.chat_id, 'Hello!'));
     telegramBot.on('/openissue', async (message: TDLTypes.message, argument: string[]) => {
-        if (message.reply_to_message_id !== 0 && argument.length !== 0) {
-            const rep = await telegramBot.getRepliedMessage(message.chat_id, message.id);
-            if (rep.content._ !== 'messageText') return;
-            const res = await githubBot.openNewIssue({
-                title: argument[0],
-                body: rep.content.text.text,
-            });
-            telegramBot.sendMessage(message.chat_id, `Open issue #${res.number} success!`);
+        if (message.reply_to_message_id === 0) {
+            return telegramBot.sendMessage(message.chat_id, 'Usege: reply some message: /openissue + issue title');
         }
+        const arg = argument.join(' ');
+        if (arg.length === 0) return await telegramBot.sendMessage(message.chat_id,
+            'Usege: /openissue + issue number');
+        const rep = await telegramBot.getRepliedMessage(message.chat_id, message.id);
+        if (rep.content._ !== 'messageText') {
+            return telegramBot.sendMessage(message.chat_id, 'Usege: reply some message: /openissue + issue number');
+        }
+        const res = await githubBot.openNewIssue({
+            title: arg,
+            body: rep.content.text.text,
+        });
+        return await telegramBot.sendMessage(message.chat_id, `Open issue #${res.number} successfully!`);
     });
-    telegramBot.on('/closeissue', async (message: TDLTypes.message, argument: string[]) => {
-        const issueNumberStr = argument[0];
+    telegramBot.on('/closeissue', async (message: TDLTypes.message, argument: string) => {
+        const issueNumberStr = argument;
         const issueNumber = Number(issueNumberStr.replace('#', ''));
         if (issueNumber === NaN) {
             return await telegramBot.sendMessage(message.chat_id, 'Usege: /closeissue + issue number');
@@ -35,7 +41,7 @@ const main = async (): Promise<void> => {
         }
         const res = await githubBot.closeIssue(issueNumber);
         if (res.state === 'closed') {
-            return await telegramBot.sendMessage(message.chat_id, `Close issue #${issueNumber} success!`);
+            return await telegramBot.sendMessage(message.chat_id, `Close issue #${issueNumber} successfully!`);
         }
     });
     telegramBot.listen();
